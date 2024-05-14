973bb510c9fd659f10cdac3df6a05723afcba841
dullorama <dtn-noreply@github.com>
2024-05-14 22:42:39 +0200

    Add password registration

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -5,6 +5,8 @@ defmodule Rumbl.Accounts.User do
   schema "users" do
     field :name, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps()
   end
@@ -15,4 +17,20 @@ defmodule Rumbl.Accounts.User do
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 3, max: 20)
   end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 90)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
+
 end
--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Rumbl.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:bcrypt_elixir, ">= 0.0.0"}
     ]
   end
 
