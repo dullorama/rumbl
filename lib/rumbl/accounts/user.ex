444ba5abb95d095d65b5cf510a851c01e5cda0e3
dullorama <dn.tan7388@gmail.com>
2024-05-14 22:10:36 +0200

    first commit

---
+++ lib/rumbl/accounts/user.ex
@@ -0,0 +1,18 @@
+defmodule Rumbl.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  def changeset(user, params) do
+    user
+    |> cast(params, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 3, max: 20)
+  end
+end
---
+++ priv/repo/seeds.exs
@@ -0,0 +1,23 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     Rumbl.Repo.insert!(%Rumbl.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
+
+alias Rumbl.Accounts.User
+alias Rumbl.Repo
+users = [
+  %User{name: "José", username: "josevalim"},
+  %User{name: "Bruce", username: "redrapids"},
+  %User{name: "Chris", username: "chrismccord"}
+]
+
+for user <- users do
+  Repo.insert user
+end
